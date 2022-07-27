0c160129e673b864ab2ec2481678210159c53ece
daryud <darinformax@gmail.com>
2022-07-26 21:53:34 -0300

    criacao da estrutura inicial e estilizacao do header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import './styles/global.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
