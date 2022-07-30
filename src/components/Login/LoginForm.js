8ceda2adbd15ed863752ec75018dc7d99678776f
daryud <darinformax@gmail.com>
2022-07-30 15:45:49 -0300

    criado helper para auxiliar em requisicoes fetch

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TOKEN_POST, USER_GET } from '../../helpers/api-fetch';
 import { useForm } from '../../hooks/useForm';
 import Button from '../Forms/Button/Button';
 import Input from '../Forms/Input/Input';
@@ -8,22 +9,34 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(e) {
+  React.useEffect(() => {
+    const token = window.sessionStorage.getItem('token');
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(e) {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username.value,
-          password: password.value,
-        }),
-      })
-        .then((r) => r.json())
-        .then((json) => console.log(json));
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+
+      window.sessionStorage.setItem('token', json.token);
+      getUser(json.token);
     }
   }
 
