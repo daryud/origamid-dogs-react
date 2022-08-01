c5db21b1f65b1f280b64e907bf4e2aafdb079fa4
darinformax@gmail.com <darinformax@gmail.com>
2022-08-01 12:49:06 -0300

    adicionando estilos ao loginForm

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../../contexts/UserContext';
-import { useForm } from '../../hooks/useForm';
-import Button from '../Forms/Button/Button';
-import Input from '../Forms/Input/Input';
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import { useForm } from "../../hooks/useForm";
+import { Error } from "../Error/Error";
+import Button from "../Forms/Button/Button";
+import Input from "../Forms/Input/Input";
+
+import styles from "./LoginForm.module.css";
+import stylesButton from "../Forms/Button/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
@@ -19,9 +23,9 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -29,9 +33,18 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.lost} to="/login/perdeu">
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.register}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+      </div>
+      <Link className={stylesButton.button} to="/login/criar">
+        Cadastro
+      </Link>
     </section>
   );
 };
